6dd59f3bd13d9dfba028419c1dbe85a92ff1dcf8
Peter Pfeufer <info@ppfeufer.de>
2024-03-17 18:42:40 +0100

    [CHANGE] Switch to dynamic versioning

--- tnnt_discordbot_cogs/__init__.py
+++ tnnt_discordbot_cogs/__init__.py
@@ -2,10 +2,5 @@
 Application init
 """
 
-# Standard Library
-from importlib import metadata
-
-__version__ = metadata.version(distribution_name="tnnt-discordbot-cogs")
+__version__ = "0.5.0"
 __title__ = "TN-NT Discordbot Cogs"
-
-del metadata
